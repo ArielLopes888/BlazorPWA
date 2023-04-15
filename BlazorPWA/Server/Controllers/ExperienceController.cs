87507d715043afcf049d48a8f7debfbdb8f90ae9
ArielLopes888 <ariellopes888@gmail.com>
2023-04-14 23:06:24 -0300

    controller retornando dados fakes para page razor

--- BlazorPWA/Server/Controllers/ExperienceController.cs
+++ BlazorPWA/Server/Controllers/ExperienceController.cs
@@ -1,35 +1,45 @@
-using BlazorPWA.Shared;
 using Microsoft.AspNetCore.Mvc;
+using BlazorPWA.Shared;
 
-namespace BlazorPWA.Server.Controllers
+namespace xx.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ExperienceController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class ExperienceController : ControllerBase
+    private readonly ILogger<ExperienceController> _logger;
+
+    public ExperienceController(ILogger<ExperienceController> logger)
     {
-        private static readonly string[] Summaries = new[]
-        {""};
-
-        private readonly ILogger<ExperienceController> _logger;
-
-        public ExperienceController(ILogger<ExperienceController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<Shared.Experience> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new Experience
-            {
-                Company = "",
-                Description = "",
-                //EndDate = DateTime.Today,
-                Position = "",
-                //StartDate = DateTime.Today,
-                
-            })
-            .ToArray();
-        }
+        _logger = logger;
     }
+
+    [HttpGet]
+    public IEnumerable<Experience> Get()
+{
+    var experiences = new List<Experience>
+    {
+        new Experience 
+        {
+            Position = "BackEnd .NET C#",
+            Company = "Mercado Eletrônico",
+            Description = "Criação de REST APIs para implementar funcionalidades em Aplicações Web de uso interno e externo.-Manutenções evolutivas e corretivas em API's.-Identificação e correções de bugs-Implementação de novos recursos e manutenção de Aplicações Web MVC -Realização de testes unitários com Nunit.-Manutenção em Microsserviços em Java, com RabbitMQ e Kafka-Manutenção em Front com Vue.js, Html e Css"
+        },
+        new Experience 
+        {
+            Position = "FrontEnd",
+            Company = "Empresa X",
+            Description = "Desenvolvimento de interfaces web responsivas e dinâmicas utilizando ReactJS, HTML e CSS. Integração com API RESTful. Utilização de metodologias ágeis como SCRUM e KANBAN."
+        },
+        new Experience 
+        {
+            Position = "Analista de Sistemas",
+            Company = "Empresa Y",
+            Description = "Análise e desenvolvimento de sistemas web utilizando ASP.NET e C#. Desenvolvimento de rotinas de banco de dados em SQL Server. Elaboração de documentação técnica e suporte aos usuários."
+        }
+    };
+
+    return experiences;
+}
+
 }
