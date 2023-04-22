89d7b279f1ae29c2933da7840d743559a9f48df3
ArielLopes888 <ariellopes888@gmail.com>
2023-04-21 22:39:40 -0300

    editando dados mocados

--- BlazorPWA/Server/Controllers/ExperienceController.cs
+++ BlazorPWA/Server/Controllers/ExperienceController.cs
@@ -14,29 +14,37 @@ public class ExperienceController : ControllerBase
         {
             new Experience
             {
-                Position = "BackEnd .NET C#",
+                Position = "BackEnd C# .NET",
                 Company = "Mercado Eletrônico",
-                Description = "Criação de REST APIs para implementar funcionalidades em Aplicações Web de uso interno e externo."+
-                "Manutenções evolutivas e corretivas em API's. Identificação e correções de bugs."+
-                "Implementação de novos recursos e manutenção de Aplicações Web MVC"+
-                "Realização de testes unitários com Nunit. Manutenção em Microsserviços em Java, com RabbitMQ e Kafka"+
-                "Manutenção em Front com Vue.js, Html e Css"
+                Description = "Criação de REST APIs para implementar funcionalidades em Aplicações Web de uso interno e externo; "+
+                "Manutenções evolutivas e corretivas em API's. Identificação e correções de bugs; "+
+                "Implementação de novos recursos e manutenção de Aplicações Web MVC; "+
+                "Realização de testes unitários com Nunit. Manutenção em Microsserviços em Java, com RabbitMQ e Kafka; "+
+                "Manutenção em Front com Vue.js, Html e Css."
             },
             new Experience
             {
-                Position = "FrontEnd",
-                Company = "Empresa X",
-                Description = "Desenvolvimento de interfaces web responsivas e dinâmicas utilizando ReactJS, HTML e CSS. Integração com API RESTful. Utilização de metodologias ágeis como SCRUM e KANBAN."
+                Position = "Assistente de Informática",
+                Company = "Mcrolins",
+                Description = "Acompanhar e monitorar as turmas de informática básica/avançada, para crianças, adolescentes e adultos; "+
+                "Avaliar os alunos, preparar materiais e planejar as  aulas; "+
+                "Implementar e realizar a manutenção de software e hardware; "+
+                 "Orientar e ensinar, em média 15 alunos por turma;"+
+                 "Realizar a manutenção de rede."
             },
             new Experience
             {
-                Position = "Analista de Sistemas",
-                Company = "Empresa Y",
-                Description = "Análise e desenvolvimento de sistemas web utilizando ASP.NET e C#. Desenvolvimento de rotinas de banco de dados em SQL Server. Elaboração de documentação técnica e suporte aos usuários."
+                Position = "Instrutor de Informática",
+                Company = "Gomes Informática",
+                Description = "Avaliar os alunos, preparar materiais e planejar as aulas; "+
+                "Implementar e realizar a manutenção de software e hardware; "+
+                "Ministrar aulas de informática básica/avançada, para crianças, adolescentes e adultos; "+
+                "Realizar a manutenção de rede; "+
+                "Orientar e ensinar, em média 20 alunos por turma."
+                
             }
         };
-
-        return experiences;
+       return experiences;
     }
 
 }
