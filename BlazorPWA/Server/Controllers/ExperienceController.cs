c32c9cfaa9c396c025f5fda1c03d1fff08539885
ArielLopes888 <ariellopes888@gmail.com>
2023-04-22 01:06:38 -0300

    correção

--- BlazorPWA/Server/Controllers/ExperienceController.cs
+++ BlazorPWA/Server/Controllers/ExperienceController.cs
@@ -17,7 +17,7 @@ public class ExperienceController : ControllerBase
                 Position = "BackEnd C# .NET",
                 Company = "Mercado Eletrônico",
                 Description = "Criação de REST APIs para implementar funcionalidades em Aplicações Web de uso interno e externo; "+
-                "Manutenções evolutivas e corretivas em API's. Identificação e correções de bugs; "+
+                "Manutenções evolutivas e corretivas em API's; Identificação e correções de bugs; "+
                 "Implementação de novos recursos e manutenção de Aplicações Web MVC; "+
                 "Realização de testes unitários com Nunit. Manutenção em Microsserviços em Java, com RabbitMQ e Kafka; "+
                 "Manutenção em Front com Vue.js, Html e Css."
@@ -25,7 +25,7 @@ public class ExperienceController : ControllerBase
             new Experience
             {
                 Position = "Assistente de Informática",
-                Company = "Mcrolins",
+                Company = "Microlins",
                 Description = "Acompanhar e monitorar as turmas de informática básica/avançada, para crianças, adolescentes e adultos; "+
                 "Avaliar os alunos, preparar materiais e planejar as  aulas; "+
                 "Implementar e realizar a manutenção de software e hardware; "+
