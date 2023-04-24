385739033855b84618541a72689def6f53baffdb
ArielLopes888 <ariellopes888@gmail.com>
2023-04-24 01:48:43 -0300

    descrição dos projetos

--- BlazorPWA/Server/Controllers/ProjectController.cs
+++ BlazorPWA/Server/Controllers/ProjectController.cs
@@ -15,19 +15,32 @@ public class ProjectController : ControllerBase
             new Project
             {
                 Name = "Api ShopBridge",
-                Description="RESTAPI",
+                Description="Esse projeto foi feito como parte de um desafio da ThinkBridge."+
+                            "A API foi escrita em C# e inclui: CRUD para gerenciar produtos em um shopping online."+
+                            "ORM Entity Framework"+
+                            "Autenticação e autorização com JWT."+
+                            "Documentação do Swagger para fácil exploração e teste de API."+
+                            "Segue os princípios de design da API RESTful."+
+                            "Utiliza as melhores práticas para desenvolvimento .NET, incluindo injeção de dependência, acesso a dados com Entity Framework Core e tratamento de exceções.",
                 Link="https://github.com/ArielLopes888/API_ShopBridge"
             },
             new Project
             {
                 Name="Api Robusta + Deploy no Azure",
-                Description="API Curso",
+                Description="Aprendendo a construir uma API robusta e moderna utilizando .NET. Curso do Lucas Eschechola"+
+                            "A API foi escrita em C# e inclui: CRUD"+
+                            "ORM Entity Framework"+
+                            "Autenticação e autorização com JWT."+
+                            "Documentação do Swagger para fácil exploração e teste de API."+
+                            "Segue os princípios de design da API RESTful."+
+                            "Utiliza as melhores práticas para desenvolvimento .NET, incluindo injeção de dependência, acesso a dados com Entity Framework Core e tratamento de exceções.",
                 Link="https://github.com/ArielLopes888/API_ROBUSTA"
             },
             new Project
             {
                 Name="Api Projeto Integrador",
-                Description="Api Curso",
+                Description="Api feita para o projeto de conlusão do Curso Desenvolvedor Web Full Stack"+
+                "A api contém várias entidades e relacionamentos Muito para Muitos",
                 Link="https://github.com/ArielLopes888/API.NET-Projeto-Integrador"
             },
             new Project
@@ -39,19 +52,25 @@ public class ProjectController : ControllerBase
             new Project
             {
                 Name="Este Portfólio",
-                Description="Blazor",
+                Description="Portfólio feito com Blazor. Tecnologias usadas: C# .Net, Sintaxe Razor, Html e Css. "+
+                            "Este projeto está dividido em 3 camadas: "+
+                            "-Camada do Cliente (Client): Responsável pela interface do usuário. O código nesta camada é executado no navegador do usuário. "+
+                            "-Camada Compartilhada (Shared): Contém o código que é compartilhado entre o cliente e o servidor. Nesse projeto contém as Entidades. "+
+                            "-Camada do Servidor (Server): Responsável por processar as solicitações do cliente e executar a lógica do aplicativo. O código nesta camada é executado no servidor e é usado para acessar bancos de dados (Nesse projeto, as controllers retrnam dados mocados para o clints).",
                 Link="https://github.com/ArielLopes888/BlazorPWA"
             },
             new Project
             {
                 Name="Python Bot Whatsapp",
-                Description="...",
+                Description="Envio automatizado de mensagens em massa no whatsapp."+
+                             "Tecnologias utilizadas: Python, Pandas, Selenium.",
                 Link="https://github.com/ArielLopes888/Automacao_Whatsapp_Python"
             },
             new Project
             {
                 Name="Automação Análise de Dados ",
-                Description="Python",
+                Description="Envio automatizado de e-mails em massa e análise e manipulação de dados; "+
+                            "Tecnologias utilizadas: Python, Pandas, Pyautogui",
                 Link="https://github.com/ArielLopes888/AnaliseDados_Automacao_Python"
             },
         };
