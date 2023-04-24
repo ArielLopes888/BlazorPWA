b69ce91f3866def91da6112bb7c8a34544855095
ArielLopes888 <ariellopes888@gmail.com>
2023-04-24 02:46:25 -0300

    add links projetos

--- BlazorPWA/Server/Controllers/ProjectController.cs
+++ BlazorPWA/Server/Controllers/ProjectController.cs
@@ -15,37 +15,44 @@ public class ProjectController : ControllerBase
             new Project
             {
                 Name = "Api ShopBridge",
-                Description="RESTAPI"
+                Description="RESTAPI",
+                Link="https://github.com/ArielLopes888/API_ShopBridge"
             },
             new Project
             {
                 Name="Api Robusta + Deploy no Azure",
-                Description="API Curso"
+                Description="API Curso",
+                Link="https://github.com/ArielLopes888/API_ROBUSTA"
             },
             new Project
             {
                 Name="Api Projeto Integrador",
                 Description="Api Curso",
+                Link="https://github.com/ArielLopes888/API.NET-Projeto-Integrador"
             },
             new Project
             {
                 Name="Cadastro de Voluntários da Congregação",
                 Description="...",
+                Link="https://github.com/ArielLopes888/Cadastro-de-Voluntarios-da-Congregacao"
             },
             new Project
             {
-                Name="Est Portfólio",
+                Name="Este Portfólio",
                 Description="Blazor",
+                Link="https://github.com/ArielLopes888/BlazorPWA"
             },
             new Project
             {
                 Name="Python Bot Whatsapp",
                 Description="...",
+                Link="https://github.com/ArielLopes888/Automacao_Whatsapp_Python"
             },
             new Project
             {
                 Name="Automação Análise de Dados ",
                 Description="Python",
+                Link="https://github.com/ArielLopes888/AnaliseDados_Automacao_Python"
             },
         };
         return values;
