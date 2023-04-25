8daac589a398d8f2fc18cbdc6b1c1395977debcc
ArielLopes888 <ariellopes888@gmail.com>
2023-04-24 23:14:51 -0300

    add dados

--- BlazorPWA/Server/Controllers/ExperienceController.cs
+++ BlazorPWA/Server/Controllers/ExperienceController.cs
@@ -20,7 +20,9 @@ public class ExperienceController : ControllerBase
                 "Manutenções evolutivas e corretivas em API's; Identificação e correções de bugs; "+
                 "Implementação de novos recursos e manutenção de Aplicações Web MVC; "+
                 "Realização de testes unitários com Nunit. Manutenção em Microsserviços em Java, com RabbitMQ e Kafka; "+
-                "Manutenção em Front com Vue.js, Html e Css."
+                "Manutenção em Front com Vue.js, Html e Css.",
+                StartDate= new DateTime(2022,06,01),
+                EndDate = new DateTime(2023,03,16)
             },
             new Experience
             {
@@ -30,7 +32,9 @@ public class ExperienceController : ControllerBase
                 "Avaliar os alunos, preparar materiais e planejar as  aulas; "+
                 "Implementar e realizar a manutenção de software e hardware; "+
                  "Orientar e ensinar, em média 15 alunos por turma;"+
-                 "Realizar a manutenção de rede."
+                 "Realizar a manutenção de rede.",
+                 StartDate= new DateTime(2016,11,01),
+                 EndDate= new DateTime(2017,05,31)
             },
             new Experience
             {
@@ -40,7 +44,9 @@ public class ExperienceController : ControllerBase
                 "Implementar e realizar a manutenção de software e hardware; "+
                 "Ministrar aulas de informática básica/avançada, para crianças, adolescentes e adultos; "+
                 "Realizar a manutenção de rede; "+
-                "Orientar e ensinar, em média 20 alunos por turma."
+                "Orientar e ensinar, em média 20 alunos por turma.",
+                StartDate= new DateTime(2011,07,01),
+                EndDate= new DateTime(203,06,01)
                 
             }
         };
