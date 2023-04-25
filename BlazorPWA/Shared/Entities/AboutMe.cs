0760e402d29c0e5e2495e216028186bed4c3e457
ArielLopes888 <ariellopes888@gmail.com>
2023-04-24 23:16:01 -0300

    ...

--- BlazorPWA/Shared/Entities/AboutMe.cs
+++ BlazorPWA/Shared/Entities/AboutMe.cs
@@ -7,11 +7,11 @@ namespace BlazorPWA.Shared
 {
     public class AboutMe
     {
-        public string address { get; set; }
-        public string phone { get; set; }
-        public string email { get; set; }
-        public string personalResume { get; set; }
-        public string professionalResume { get; set; }
+        public string? address { get; set; }
+        public string? phone { get; set; }
+        public string? email { get; set; }
+        public string? personalResume { get; set; }
+        public string? professionalResume { get; set; }
 
     }
 }
--- BlazorPWA/Shared/Entities/Project.cs
+++ BlazorPWA/Shared/Entities/Project.cs
@@ -7,8 +7,8 @@ namespace BlazorPWA.Shared
 {
  public class Project
     {
-        public string Name { get; set; }
-        public string Link { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? Link { get; set; }
+        public string? Description { get; set; }
     }
 }
