047eaf105613dbb4825b346a5ca772ebe616c3ee
ArielLopes888 <ariellopes888@gmail.com>
2023-04-24 23:14:17 -0300

    ..

--- BlazorPWA/Shared/Entities/Experience.cs
+++ BlazorPWA/Shared/Entities/Experience.cs
@@ -2,11 +2,11 @@ namespace BlazorPWA.Shared
 {
     public class Experience
     {
-        public string Position { get; set; }
-        public string Company { get; set; }
+        public string? Position { get; set; }
+        public string? Company { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public string Description { get; set; }
-        public string LanguagesTools { get; set; }
+        public string? Description { get; set; }
+        public string? LanguagesTools { get; set; }
     }
 }
