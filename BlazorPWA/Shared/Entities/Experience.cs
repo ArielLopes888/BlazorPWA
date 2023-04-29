25632d701043ee3e3e117bc7168c474a7f098d09
ArielLopes888 <ariellopes888@gmail.com>
2023-04-28 23:09:45 -0300

    removendo propriedade

--- BlazorPWA/Shared/Entities/Experience.cs
+++ BlazorPWA/Shared/Entities/Experience.cs
@@ -7,6 +7,5 @@ namespace BlazorPWA.Shared
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string? Description { get; set; }
-        public string? LanguagesTools { get; set; }
     }
 }
